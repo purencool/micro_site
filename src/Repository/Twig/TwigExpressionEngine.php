d692c50eed3d630a85bebfc0afb3f45d01a97131
Purencool <purencool@gmail.com>
2022-09-21 16:45:41 +1000

    Twig expression engine structure

--- src/Repository/Twig/TwigExpressionEngine.php
+++ src/Repository/Twig/TwigExpressionEngine.php
@@ -2,6 +2,10 @@
 
 namespace App\Repository\Twig;
 
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+
 /**
  * Description of TwigExpressionEngine
  *
@@ -10,38 +14,72 @@ namespace App\Repository\Twig;
 class TwigExpressionEngine {
 
   /**
+   * Expression engine array.
+   * 
+   * @var array $expression
+   */
+  protected static $expression = [
+     '@string' => '<expression_variable>',
+     '@extend' => "{% extends '<expression_variable>' %}" 
+  ];
+
+  /**
+   * Adding twig configuration to the file.
+   * 
+   * @param type $contents
+   *    Configuration for the template.
+   * @param type $path
+   *    Path to add configuration.
+   */
+  protected static function addToFile($contents, $path)
+  {
+    $filesystem = new Filesystem();
+      foreach ($contents['@config'] as $config_key => $config_value) {
+        if(!is_array($config_value)){
+          if($config_key == '@extend') {
+            $stringToReplace = self::$expression['@extend'];
+            $twig = str_replace(self::$expression['@string'], $config_value, $stringToReplace);
+            $filesystem->appendToFile($path.$contents['@creation'],$twig, true);
+          }
+        }
+      } 
+  }
+
+  /**
+   * Testing to see if the configuration files is for twig.
    * 
    * @param type $path
+   *    Path of twig configuration directory.
+   * @param type $fileName
+   *    File name of the configuration file.
    */
-  protected static function templateCreation($path) {
-     echo $path . "<br>";
-    // echo "<pre>";
-     echo file_get_contents($path);
-    // echo "</pre>";
-    // echo "<br>";
-    // echo "<pre>";
-    // print_r(json_decode(file_get_contents($path)));
-    // echo "</pre>";
-    // echo "<br>";
-    // echo "<br>";
+  protected static function templateCreation($path,$fileName) 
+  {
+
+    $filesystem = new Filesystem();
+    $pathFileName = $path.$fileName;
+    $contents = json_decode(file_get_contents($pathFileName), true);
+    if (isset($contents['@type']) && $contents['@type'] === 'twig' && isset($contents['@config'])) {
+      $filesystem->dumpFile($path.$contents['@creation'],'{# Created by Twig Expresion Engine #}');
+      self::addToFile($contents, $path);
+    }
   }
 
   /**
    * 
    * @param type $path
    */
-  protected static function recursiveCopy() {
+  protected static function recursiveCopy() 
+  {
     $ds = DIRECTORY_SEPARATOR;
     $src = __DIR__ . $ds . ".." . $ds . ".." . $ds . ".." . $ds . "templates" . $ds . "layouts";
     $dir = opendir($src);
     while (( $file = readdir($dir))) {
-      if (( $file != '.' ) && ( $file != '..' )) {
-        if (\is_dir($src . $ds . $file)) {
-          $filesArray = scandir($src . $ds . $file);
-          foreach ($filesArray as $fileName) {
-            if (( $fileName != '.' ) && ( $fileName != '..' )) {
-              self::templateCreation($src . $ds . $file . $ds . $fileName);
-            }
+      if (( $file != '.' ) && ( $file != '..' ) && \is_dir($src . $ds . $file)) {
+        $filesArray = scandir($src . $ds . $file);
+        foreach ($filesArray as $fileName) {
+          if (( $fileName != '.' ) && ( $fileName != '..' )) {
+            self::templateCreation($src . $ds . $file . $ds , $fileName);
           }
         }
       }
@@ -55,7 +93,8 @@ class TwigExpressionEngine {
    * @param type $path
    *    Path to directory that has Json configuration.
    */
-  public static function create() {
+  public static function create() 
+  {
     self::recursiveCopy();
   }
 

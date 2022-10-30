5a9015858e056f8e0ffc0680520104d573586534
Purencool <purencool@gmail.com>
2022-10-30 18:23:08 +1100

    Creating html string for twig variable

--- src/Controller/Observers/DataLayout.php
+++ src/Controller/Observers/DataLayout.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller\Observers;
 
-use App\Repository\Processes\ContentCreation;
-use App\Repository\Processes\LayoutCreation;
-
 /**
  * Gets data array after building the content from the route.
  *
@@ -12,12 +9,15 @@ use App\Repository\Processes\LayoutCreation;
  */
 class DataLayout {
 
-/**
- * 
- * @param array $array
- * @return array
- */
-  private static function flatten(array $array) {
+  /**
+   * Flattens array so that in can be used in the layout.
+   * 
+   * @param array $array
+   *    Array that needs to be flattened.
+   * @return array
+   *    Flattened array.
+   */
+  private static function flatten(array $array) : array {
     $return = [];
     array_walk_recursive($array, function ($a) use (&$return) {
       $return[] = $a;
@@ -25,13 +25,18 @@ class DataLayout {
     return $return;
   }
 
-/**
- * 
- * @param type $content
- * @param type $contentReference
- * @return string
- */
-  private static function recursiveSearch($content, $contentReference) {
+  /**
+   * Test to see if the content '@data' object matches the content reference 
+   * so that it can be added to the 'layout_data_combined' array.
+   * 
+   * @param array $content
+   *     The content object is tested for key.
+   * @param type $contentReference
+   *     This is the key need to access the data.
+   * @return string
+   *     Returns content data.
+   */
+  private static function recursiveSearch($content, String $contentReference): string {
 
     foreach ($content as $contentItem) {
       if (array_key_exists($contentReference, (array) $contentItem->{'@data'})) {
@@ -42,12 +47,12 @@ class DataLayout {
     return 'Content couldn\'t not be found';
   }
 
- /**
-  * 
-  * @param type $placeholderObject
-  * @param type $data
-  * @return type
-  */
+  /**
+   * 
+   * @param type $placeholderObject
+   * @param type $data
+   * @return type
+   */
   private static function getContentForLayout($placeholderObject, $data) {
     $contentResult = self::recursiveSearch(
         self::flatten($data['data']['@data_array']['@data']),
